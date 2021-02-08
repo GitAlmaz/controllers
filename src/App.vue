5e83d59224372d410ecfb257e9a7edccdc6fa935
GitAlmaz <almazinhs@gmail.com>
2021-02-09 04:35:27 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <Controller
+        ref="ctrl1"
+        :extra="{
+          text: 'Сумма',
+          method: () => {
+            this.$refs.ctrl1.number =
+              +this.$refs.ctrl2.number + +this.$refs.ctrl3.number
+          }
+        }"
+        :tab="() => ({ next: this.$refs.ctrl2, prev: this.$refs.ctrl3 })"
+      />
+      <Controller
+        ref="ctrl2"
+        :getItem="() => this.$refs.ctrl3"
+        :extra="{
+          text: 'Константа',
+          method: () => (this.$refs.ctrl1.number = 1000)
+        }"
+        :tab="() => ({ next: this.$refs.ctrl3, prev: this.$refs.ctrl1 })"
+      />
+      <Controller
+        ref="ctrl3"
+        :getItem="() => this.$refs.ctrl2"
+        :tab="() => ({ next: this.$refs.ctrl1, prev: this.$refs.ctrl2 })"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Controller from "./components/Controller.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    Controller
+  },
+  data: () => ({}),
+  methods: {},
+  mounted() {}
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #f5f5f5;
+  background-color: #333;
+}
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/Controller.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="ctrl">
+    <div class="ctrl__preview" v-if="!open" @click="openHandler">
+      <p>{{ Number(+this.number).toLocaleString() }}</p>
+    </div>
+    <div class="ctrl__input" v-else>
+      <input
+        type="number"
+        ref="input"
+        v-model="number"
+        @input="inputHandler"
+        @blur="closeHandler"
+        @focus="$event.target.select()"
+        @submit="closeHandler"
+        @keyup.enter="closeHandler"
+        @keyup.esc="exitHandler"
+        @keydown.tab.prevent="e => handleTabPress(e)"
+      />
+      <a href="#" v-if="extra" @mousedown="extra.method">
+        {{ extra.text }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+/** 
+ * Надеюсь можно использовать намбер инпут
+ * при нажатии на "константа" я так понял в первый контролл нужно вписывать 1000
+ * при нажатии на "сумма" в первый контрол вписывать значение суммы из 2-го и 3-го
+ * за чрезмерное использование рефов извиняюсь, знаю, что не бест практис :с
+ * */ 
+export default {
+  props: {
+    changeHandler: {
+      type: Function,
+      required: false
+    },
+    getItem: {
+      type: Function
+    },
+    extra: {
+      type: Object
+    },
+    tab: {
+      type: Function
+    }
+  },
+  data: () => ({
+    open: false,
+    number: 0,
+    prevNum: null
+  }),
+  methods: {
+    openHandler() {
+      this.open = true
+      this.prevNum = this.number
+    },
+    inputHandler() {
+      if (this.number < 0) {
+        this.number = 0
+      }
+      if (this.getItem) {
+        const item = this.getItem()
+        item.number = this.number
+      }
+    },
+    closeHandler() {
+      if (!this.number) {
+        this.number = 0
+      }
+      this.open = false
+    },
+    exitHandler(e) {
+      this.number = this.prevNum
+      this.open = false
+    },
+    handleTabPress(e) {
+      const item = this.tab()
+      if (!e.shiftKey) {
+        item.next.open = true
+      } else {
+        item.prev.open = true
+      }
+    }
+  },
+  updated() {
+    if (this.open) {
+      this.$refs.input.focus()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.ctrl {
+  font-size: 24px;
+  margin: 15px 0;
+  &__preview {
+    position: relative;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    transition: 0.3s;
+    padding: 3px 15px;
+    border: 1px solid #fff;
+    border-radius: 3px;
+    &::after {
+      content: ">";
+      font-size: 18px;
+      transform: rotate(90deg);
+      margin-left: 10px;
+    }
+    &:hover {
+      color: #7187ff;
+      border-color: #7187ff;
+    }
+  }
+  &__input {
+    position: relative;
+    input {
+      width: 120px;
+      outline: none;
+      appearance: none;
+      border: 1px solid #7187ff;
+      border-radius: 3px;
+      padding: 3px 3px 3px 15px;
+      font-size: 24px;
+      background-color: transparent;
+      color: #7187ff;
+    }
+    a {
+      font-size: 12px;
+      color: #7187ff;
+      position: absolute;
+      left: 110%;
+      top: 0;
+    }
+  }
+}
+</style>
